c51c466044952360b90fcbd0b41f9b0445b4275a
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2023-09-05 10:29:13 +0700

    feat: Add Partytown + Google Analytics

--- src/root.tsx
+++ src/root.tsx
@@ -5,6 +5,7 @@ import {
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
+import { QwikPartytown } from "./components/partytown/partytown";
 
 import "./global.css";
 
@@ -19,13 +20,33 @@ export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
-        <meta charSet="utf-8" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
       <body lang="en">
         <RouterOutlet />
         <ServiceWorkerRegister />
+        <QwikPartytown forward={["dataLayer.push"]} />
+        {import.meta.env.PUBLIC_GA_MEASUREMENT_ID && (
+          <>
+            <script
+              async
+              type="text/partytown"
+              src={`https://www.googletagmanager.com/gtag/js?id=${
+                import.meta.env.PUBLIC_GA_MEASUREMENT_ID
+              }`}
+            />
+            <script
+              type="text/partytown"
+              dangerouslySetInnerHTML={`window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${import.meta.env.PUBLIC_GA_MEASUREMENT_ID}');`}
+            />
+          </>
+        )}
       </body>
     </QwikCityProvider>
   );
