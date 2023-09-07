ecf5802fed2ecdcc259b94255998b809419d6ae7
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2023-09-07 10:54:19 +0700

    refactor: Disable zoom in PWA

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -13,7 +13,10 @@ export const RouterHead = component$(() => {
       <title>{head.title}</title>
 
       <link rel="canonical" href={loc.url.href} />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      />
       <link rel="apple-touch-icon" href="/icon/ios/180.png" />
       {[16, 32, 64, 144, 192, 256, 512].map((size) => (
         <link
