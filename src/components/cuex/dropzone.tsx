19661b69d462627364ec47707589ea17002c92e6
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2023-09-12 10:27:08 +0700

    refactor: Remove border dashboard

--- src/components/cuex/dropzone.tsx
+++ src/components/cuex/dropzone.tsx
@@ -9,7 +9,7 @@ export const Dropzone = component$<DropzoneProps>(({ onChange$ }) => {
   return (
     <label
       for="dropzone-file"
-      class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+      class="flex flex-col items-center justify-center w-full h-64 rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
     >
       <div class="flex flex-col items-center justify-center pt-5 pb-6">
         <LuFileText class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" />
