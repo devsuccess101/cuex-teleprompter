758f678314050da1d8c4348f1f4c914500f7fb1a
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2023-09-19 23:07:29 +0700

    refactor: Update router-head.tsx

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,8 +15,10 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        content="width=device-width, initial-scale=1.0, viewport-fit=cover"
       />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta
         name="google-site-verification"
         content="yu60NP-CTckMJK8rCQBg1xrNZ75rcqoxlg0Pz3-spfo"
